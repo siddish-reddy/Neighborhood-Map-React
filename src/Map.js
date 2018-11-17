3ed3d84bcff93ae2b630fbb96419e7392924d2f1
siddish reddy <Siddish.ibv@gmail.com>
2018-11-17 19:39:37 +0530

    added details on where to replace API keys

--- src/Map.js
+++ src/Map.js
@@ -31,6 +31,7 @@ const MapHelper = withScriptjs(withGoogleMap((props) =>
 class Map extends React.Component {
     render() {
         console.log(this.props.venueMarkers)
+        // Replace with your google maps API 
         return (
             <div className='map'>
                 <MapHelper 
